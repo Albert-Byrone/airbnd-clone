0718b6f460bfff1332fc368928635b17b8052a71
Albert-Byrone <albertbyrone1677@gmail.com>
2021-12-31 19:15:19 +0300

    add the banner

---
+++ src/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <div className="banner">
+      <p>This is the banner page</p>
+    </div>
+  );
+}
+
+export default Banner;
