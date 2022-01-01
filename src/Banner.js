f08f0d17b2b7ab3f80fcc25eb63de6542cbcd425
Albert-Byrone <albertbyrone1677@gmail.com>
2022-01-01 15:28:08 +0300

    add the content on the banner page

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Banner.css";
+import Button from "@mui/material/Button";
+import Search from "./Search";
 
 function Banner() {
+  const [showSearch, setshowSearch] = useState(false);
   return (
     <div className="banner">
       <div className="banner__search">
@@ -12,6 +15,12 @@ function Banner() {
           variant="outlined"
         >
           Search Date
+        </Button>
+      </div>
+      <div className="banner__info">
+        <h1 className="">Not sure where to go? Perfect. </h1>
+        <Button variant="outlined" size="large">
+          I'm flexible
         </Button>
       </div>
     </div>
