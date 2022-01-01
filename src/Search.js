04085ca23d50c967e0f5f5a5ad3db5c08a64198b
Albert-Byrone <albertbyrone1677@gmail.com>
2022-01-01 15:42:03 +0300

    create an object to store the variables

--- src/Search.js
+++ src/Search.js
@@ -1,2 +1,9 @@
   const [startDate, setstartDate] = useState(new Date());
   const [endDate, setendDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
