6e119ecbe6360e71b4302735cc01c5262579abc6
Albert-Byrone <albertbyrone1677@gmail.com>
2022-01-01 15:43:55 +0300

    add the date picker

--- src/Search.js
+++ src/Search.js
@@ -1,3 +1,14 @@
+import React, { useState } from "react";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css";
+import PeopleIcon from "@mui/icons-material/People";
+import Button from "@mui/material/Button";
+
+// theme css file
+import { DateRangePicker } from "react-date-range";
+import "./Search.css";
+
+function Search() {
   const [startDate, setstartDate] = useState(new Date());
   const [endDate, setendDate] = useState(new Date());
 
@@ -11,9 +22,17 @@
     setstartDate(ranges.selection.startDate);
     setendDate(ranges.selection.endDate);
   }
+  return (
+    <div className="search">
+      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
       <h2>
         Number of guests
         <PeopleIcon />
       </h2>
       <input type="number" min={0} defaultValue={2} />
       <Button>Search Airbnb</Button>
+    </div>
+  );
+}
+
+export default Search;
