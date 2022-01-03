b7526d4f18438d88ecb5fcd5a241a2254c84fccb
Albert-Byrone <albertbyrone1677@gmail.com>
2022-01-03 17:22:24 +0300

    minor updates

--- src/Banner.js
+++ src/Banner.js
@@ -6,22 +6,24 @@ import Search from "./Search";
 function Banner() {
   const [showSearch, setshowSearch] = useState(false);
   return (
-    <div className="banner">
-      <div className="banner__search">
-        {showSearch && <Search />}
-        <Button
-          onClick={() => setshowSearch(!showSearch)}
-          className="banner__searchBtn"
-          variant="outlined"
-        >
-          {showSearch ? "Hide" : "Search Dates"}
-        </Button>
-      </div>
-      <div className="banner__info">
-        <h1 className="">Not sure where to go? Perfect. </h1>
-        <Button variant="outlined" size="large">
-          I'm flexible
-        </Button>
+    <div className="banner__container">
+      <div className="banner">
+        <div className="banner__search">
+          {showSearch && <Search />}
+          <Button
+            onClick={() => setshowSearch(!showSearch)}
+            className="banner__searchBtn"
+            variant="outlined"
+          >
+            {showSearch ? "Hide" : "Search Dates"}
+          </Button>
+        </div>
+        <div className="banner__info">
+          <h1 className="">Not sure where to go? Perfect. </h1>
+          <Button variant="outlined" size="large">
+            <span>I'm flexible</span>
+          </Button>
+        </div>
       </div>
     </div>
   );
