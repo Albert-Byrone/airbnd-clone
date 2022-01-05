a82a454f2ef7cca0cd4afe5b1eede37f6e88fb10
Albert-Byrone <albertbyrone1677@gmail.com>
2022-01-05 11:50:18 +0300

    adit the class names

--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Footer.css";
 
 function Footer() {
@@ -32,361 +32,364 @@ function Footer() {
   }
   return (
     <div className="footer">
-      <div class="tabs">
-        <div class="tabs__sidebar">
-          <button class="tabs__button tabs__button--active" data-for-tab="1">
+      <div className="tabs">
+        <div className="tabs__sidebar">
+          <button
+            className="tabs__button tabs__button--active"
+            data-for-tab="1"
+          >
             Destinations for arts & culture
           </button>
-          <button class="tabs__button" data-for-tab="2">
+          <button className="tabs__button" data-for-tab="2">
             Destinations for outdoor adventure
           </button>
-          <button class="tabs__button" data-for-tab="3">
+          <button className="tabs__button" data-for-tab="3">
             Mountain cabins
           </button>
-          <button class="tabs__button" data-for-tab="4">
+          <button className="tabs__button" data-for-tab="4">
             Beach destinations
           </button>
-          <button class="tabs__button" data-for-tab="5">
+          <button className="tabs__button" data-for-tab="5">
             Popular destinations
           </button>
-          <button class="tabs__button" data-for-tab="6">
+          <button className="tabs__button" data-for-tab="6">
             Unique Stays
           </button>
         </div>
-        <div class="tabs__content tabs__content--active" data-tab="1">
-          <div class="tabs__container">
+        <div className="tabs__content tabs__content--active" data-tab="1">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
-              <p class="tabs__par">Arizona</p>
+              <h4 className="tabs__title">Phoenix</h4>
+              <p className="tabs__par">Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="tabs__title">San Diego</h4>
+              <h4 className="tabs__title">San Diego</h4>
               <p>California</p>
             </div>
           </div>
-          <div class="tabs__container">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
+              <h4 className="tabs__title">Phoenix</h4>
               <p>Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="tabs__title">San Diego</h4>
+              <h4 className="tabs__title">San Diego</h4>
               <p>California</p>
             </div>
           </div>
-          <div class="tabs__container">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
+              <h4 className="tabs__title">Phoenix</h4>
               <p>Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="">San Diego</h4>
+              <h4 className="">San Diego</h4>
               <p>California</p>
             </div>
           </div>
         </div>
 
-        <div class="tabs__content" data-tab="2">
-          <div class="tabs__container">
+        <div className="tabs__content" data-tab="2">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
-              <p class="tabs__par">Arizona</p>
+              <h4 className="tabs__title">Phoenix</h4>
+              <p className="tabs__par">Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="tabs__title">San Diego</h4>
+              <h4 className="tabs__title">San Diego</h4>
               <p>California</p>
             </div>
           </div>
-          <div class="tabs__container">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
+              <h4 className="tabs__title">Phoenix</h4>
               <p>Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="tabs__title">San Diego</h4>
+              <h4 className="tabs__title">San Diego</h4>
               <p>California</p>
             </div>
           </div>
-          <div class="tabs__container">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
+              <h4 className="tabs__title">Phoenix</h4>
               <p>Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="">San Diego</h4>
+              <h4 className="">San Diego</h4>
               <p>California</p>
             </div>
           </div>
         </div>
 
-        <div class="tabs__content" data-tab="3">
-          <div class="tabs__container">
+        <div className="tabs__content" data-tab="3">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
-              <p class="tabs__par">Arizona</p>
+              <h4 className="tabs__title">Phoenix</h4>
+              <p className="tabs__par">Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="tabs__title">San Diego</h4>
+              <h4 className="tabs__title">San Diego</h4>
               <p>California</p>
             </div>
           </div>
-          <div class="tabs__container">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
+              <h4 className="tabs__title">Phoenix</h4>
               <p>Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="tabs__title">San Diego</h4>
+              <h4 className="tabs__title">San Diego</h4>
               <p>California</p>
             </div>
           </div>
-          <div class="tabs__container">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
+              <h4 className="tabs__title">Phoenix</h4>
               <p>Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="">San Diego</h4>
+              <h4 className="">San Diego</h4>
               <p>California</p>
             </div>
           </div>
         </div>
-        <div class="tabs__content" data-tab="4">
-          <div class="tabs__container">
+        <div className="tabs__content" data-tab="4">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
-              <p class="tabs__par">Arizona</p>
+              <h4 className="tabs__title">Phoenix</h4>
+              <p className="tabs__par">Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="tabs__title">San Diego</h4>
+              <h4 className="tabs__title">San Diego</h4>
               <p>California</p>
             </div>
           </div>
-          <div class="tabs__container">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
+              <h4 className="tabs__title">Phoenix</h4>
               <p>Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="tabs__title">San Diego</h4>
+              <h4 className="tabs__title">San Diego</h4>
               <p>California</p>
             </div>
           </div>
-          <div class="tabs__container">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
+              <h4 className="tabs__title">Phoenix</h4>
               <p>Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="">San Diego</h4>
+              <h4 className="">San Diego</h4>
               <p>California</p>
             </div>
           </div>
         </div>
-        <div class="tabs__content" data-tab="5">
-          <div class="tabs__container">
+        <div className="tabs__content" data-tab="5">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
-              <p class="tabs__par">Arizona</p>
+              <h4 className="tabs__title">Phoenix</h4>
+              <p className="tabs__par">Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="tabs__title">San Diego</h4>
+              <h4 className="tabs__title">San Diego</h4>
               <p>California</p>
             </div>
           </div>
-          <div class="tabs__container">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
+              <h4 className="tabs__title">Phoenix</h4>
               <p>Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="tabs__title">San Diego</h4>
+              <h4 className="tabs__title">San Diego</h4>
               <p>California</p>
             </div>
           </div>
-          <div class="tabs__container">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
+              <h4 className="tabs__title">Phoenix</h4>
               <p>Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="">San Diego</h4>
+              <h4 className="">San Diego</h4>
               <p>California</p>
             </div>
           </div>
         </div>
-        <div class="tabs__content" data-tab="6">
-          <div class="tabs__container">
+        <div className="tabs__content" data-tab="6">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
-              <p class="tabs__par">Arizona</p>
+              <h4 className="tabs__title">Phoenix</h4>
+              <p className="tabs__par">Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="tabs__title">San Diego</h4>
+              <h4 className="tabs__title">San Diego</h4>
               <p>California</p>
             </div>
           </div>
-          <div class="tabs__container">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
+              <h4 className="tabs__title">Phoenix</h4>
               <p>Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="tabs__title">San Diego</h4>
+              <h4 className="tabs__title">San Diego</h4>
               <p>California</p>
             </div>
           </div>
-          <div class="tabs__container">
+          <div className="tabs__container">
             <div>
-              <h4 class="tabs__title">Phoenix</h4>
+              <h4 className="tabs__title">Phoenix</h4>
               <p>Arizona</p>
             </div>
             <div>
-              <h4 class="tabs__title">Hot Springs</h4>
+              <h4 className="tabs__title">Hot Springs</h4>
               <p>Arkansas</p>
             </div>
             <div>
-              <h4 class="tabs__title">Los Angeles</h4>
+              <h4 className="tabs__title">Los Angeles</h4>
               <p>California</p>
             </div>
             <div>
-              <h4 class="">San Diego</h4>
+              <h4 className="">San Diego</h4>
               <p>California</p>
             </div>
           </div>
