9b8d02794b675efd1d4347853b91b2a1a6e159d8
Albert-Byrone <albertbyrone1677@gmail.com>
2022-01-11 10:13:21 +0300

    import the foot

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Footer from "./Footer";
+import Foot from "./Foot";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Foot />
     </div>
   );
 }
