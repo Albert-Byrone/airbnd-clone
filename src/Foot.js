db7e80fcc6023f5d277bc15fe22063e217b240a1
Albert-Byrone <albertbyrone1677@gmail.com>
2022-01-11 16:49:32 +0300

    minor updates

--- src/Foot.js
+++ src/Foot.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Foot.css";
 import LanguageIcon from "@mui/icons-material/Language";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 function Foot() {
   return (
@@ -12,9 +13,12 @@ function Foot() {
       <div className="foot__right">
         <div className="foot__rightone">
           <LanguageIcon fontSize="mediam" />
-          <>Language</>
+          <span>Language</span>
         </div>
-        <div className="foot__rightone">
+        <div className="foot__righttwo">
+          <TwitterIcon fontSize="large"/>
+          <TwitterIcon fontSize="large" />
+          <TwitterIcon fontSize="large" />
           {/* <p> © 2022 Airbnb, Inc. Demo. Privacy·Terms·Sitemap</p> */}
         </div>
       </div>
