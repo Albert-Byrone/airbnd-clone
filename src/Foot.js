1978484fd5de590c75f3b6dc5ab58f0dfa854e18
Albert-Byrone <albertbyrone1677@gmail.com>
2022-01-12 15:53:35 +0300

    add the social media icons

--- src/Foot.js
+++ src/Foot.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./Foot.css";
 import LanguageIcon from "@mui/icons-material/Language";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 function Foot() {
   return (
@@ -16,9 +18,17 @@ function Foot() {
           <span>Language</span>
         </div>
         <div className="foot__righttwo">
-          <TwitterIcon fontSize="large"/>
-          <TwitterIcon fontSize="large" />
-          <TwitterIcon fontSize="large" />
+          <ul>
+            <li>
+              <FacebookIcon fontSize="small" />
+            </li>
+            <li>
+              <InstagramIcon fontSize="small" />
+            </li>
+            <li>
+              <TwitterIcon fontSize="small" />
+            </li>
+          </ul>
           {/* <p> © 2022 Airbnb, Inc. Demo. Privacy·Terms·Sitemap</p> */}
         </div>
       </div>
