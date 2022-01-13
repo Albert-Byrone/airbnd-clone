fc1d4547733f50ce88727ba241eafffd526957f9
Albert-Byrone <albertbyrone1677@gmail.com>
2022-01-13 14:36:46 +0300

    play around with he routiing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
-import Footer from "./Footer";
+// import Footer from "./Footer";
 import Foot from "./Foot";
+// import { render } from "react-dom";
+import { Route, Router, Switch } from "react-router-dom";
+import SearchPage from "./SearchPage";
+// import your route components too
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Foot />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Foot />
+      </Router>
     </div>
   );
 }
 
 export default App;
+{
+  /* <>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<SearchPage />} />
+          </Routes>
+          <Foot />
+      </> */
+}
