4c40d9cafdaa2e07aad32e88254eeea8add82b4f
Albert-Byrone <albertbyrone1677@gmail.com>
2022-01-19 16:42:08 +0300

    add the various buttons

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,10 +1,27 @@
 import React from "react";
+import "./SearchPage.css";
+import Button from "@mui/material/Button";
 
 export default function SearchPage() {
   return (
     <div className="search__page">
-    
-      <p>Testing the page</p>
+      <div className="searchPage__info">
+        <div className="searchPage__menu">
+          <Button fontSize="medium">Tiny homes</Button>
+          <Button>Amazing pool</Button>
+          <Button>Cabins</Button>
+          <Button>A-franmes</Button>
+          <Button>Beachfront</Button>
+          <Button>Luxe</Button>
+          <Button>Farms</Button>
+          <Button>Shipping Containers</Button>
+        </div>
+        <div className="searchPage__tab">
+          <Button variant="outlined">Apply</Button>
+          <Button variant="outlined">Guest</Button>
+          <Button variant="outlined">Filter</Button>
+        </div>
+      </div>
     </div>
   );
 }
