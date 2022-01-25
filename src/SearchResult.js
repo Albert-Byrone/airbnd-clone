69695a1eaa5451f526efd779dad3f720c941701d
Albert-Byrone <albertbyrone1677@gmail.com>
2022-01-25 15:33:54 +0300

    add the result page

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -1,10 +1,22 @@
 import React from "react";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import "./SearchResult.css";
 
 function SearchResult({ img, location, title, description, price, total }) {
   return (
     <div className="searchResult">
-      <p>{img}</p>
-      <p>{location}</p>
+      <img src={img} alt="" />
+      <FavoriteBorderIcon className="serachResult__heart" />
+      <div className="serachResult__info">
+        <div className="searchResult__toTop">
+          <p>{title}</p>
+          <p>{price}</p>
+        </div>
+        <div className="searchResult__toButton">
+          <p>{title}</p>
+          <p>{price}</p>
+        </div>
+      </div>
     </div>
   );
 }
